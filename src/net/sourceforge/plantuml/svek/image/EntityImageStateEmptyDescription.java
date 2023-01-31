3514e4b8b275854c3a13daa6875594f5c716283d
David Fyfe <david.fyfe@planetinnovation.com.au>
2023-01-31 13:53:39 +1100

    Improve transition display
    Improve transition display by reducing the size of the transition
    element.  Should also be used in conjunction with setting the size
    of the transition element round corners to 0 with a skinParam setting
    such as `skinParam RoundCorner<<transition>> 0`.

--- src/net/sourceforge/plantuml/svek/image/EntityImageStateEmptyDescription.java
+++ src/net/sourceforge/plantuml/svek/image/EntityImageStateEmptyDescription.java
@@ -35,9 +35,11 @@
  */
 package net.sourceforge.plantuml.svek.image;
 
+import net.sourceforge.plantuml.Guillemet;
 import net.sourceforge.plantuml.ISkinParam;
 import net.sourceforge.plantuml.awt.geom.XDimension2D;
 import net.sourceforge.plantuml.baraye.IEntity;
+import net.sourceforge.plantuml.cucadiagram.Stereotype;
 import net.sourceforge.plantuml.graphic.StringBounder;
 import net.sourceforge.plantuml.ugraphic.UGraphic;
 import net.sourceforge.plantuml.ugraphic.UStroke;
@@ -47,26 +49,47 @@ public class EntityImageStateEmptyDescription extends EntityImageStateCommon {
 
 	final private static int MIN_WIDTH = 50;
 	final private static int MIN_HEIGHT = 40;
+	final private static int MIN_TRANSITION_WIDTH = 30;
+	final private static int MIN_TRANSITION_HEIGHT = 15;
+
+	private boolean isTransition = false;
 
 	public EntityImageStateEmptyDescription(IEntity entity, ISkinParam skinParam) {
 		super(entity, skinParam);
+		Stereotype stereotype = entity.getStereotype();
 
+		if (stereotype != null) {
+			this.isTransition = (
+				"<<transition>>".equals(stereotype.getLabel(Guillemet.DOUBLE_COMPARATOR)) ||
+				"<<transition_hidden>>".equals(stereotype.getLabel(Guillemet.DOUBLE_COMPARATOR))
+			);
+		}
 	}
 
 	public XDimension2D calculateDimension(StringBounder stringBounder) {
 		final XDimension2D dim = title.calculateDimension(stringBounder);
 		final XDimension2D result = dim.delta(MARGIN * 2);
-		return result.atLeast(MIN_WIDTH, MIN_HEIGHT);
+
+		if (this.isTransition) {
+			return result.delta(0, -12).atLeast(MIN_TRANSITION_WIDTH, MIN_TRANSITION_HEIGHT);
+		}
+		else {
+			return result.atLeast(MIN_WIDTH, MIN_HEIGHT);
+		}
 	}
 
 	final public void drawU(UGraphic ug) {
 		if (url != null)
 			ug.startUrl(url);
 
-		final StringBounder stringBounder = ug.getStringBounder();
+		final StringBounder stringBounder = ug.getStringBounder();		
+		int yShift = 0;
+
+		if (this.isTransition) {
+			yShift = -2;
+		}
 		final XDimension2D dimTotal = calculateDimension(stringBounder);
 		final XDimension2D dimDesc = title.calculateDimension(stringBounder);
-
 		final UStroke stroke = getStyleState().getStroke(lineConfig.getColors());
 
 		ug = applyColor(ug);
@@ -75,7 +98,7 @@ public class EntityImageStateEmptyDescription extends EntityImageStateCommon {
 		ug.draw(getShape(dimTotal));
 
 		final double xDesc = (dimTotal.getWidth() - dimDesc.getWidth()) / 2;
-		final double yDesc = (dimTotal.getHeight() - dimDesc.getHeight()) / 2;
+		final double yDesc = (dimTotal.getHeight() - dimDesc.getHeight()) / 2 + yShift;
 		title.drawU(ug.apply(new UTranslate(xDesc, yDesc)));
 
 		if (url != null)
